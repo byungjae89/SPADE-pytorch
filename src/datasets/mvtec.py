c240a8e3c5936cc07b6a90ce0574e996ea192d86
byungjae89 <byungjae89@gmail.com>
2020-07-15 23:13:36 +0900

    initial code commit

---
+++ src/datasets/mvtec.py
@@ -0,0 +1,110 @@
+import os
+import wget
+import tarfile
+import numpy as np
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms as T
+
+
+URL = 'ftp://guest:GU.205dldo@ftp.softronics.ch/mvtec_anomaly_detection/mvtec_anomaly_detection.tar.xz'
+CLASS_NAMES = ['bottle', 'cable', 'capsule', 'carpet', 'grid',
+               'hazelnut', 'leather', 'metal_nut', 'pill', 'screw',
+               'tile', 'toothbrush', 'transistor', 'wood', 'zipper']
+
+
+class MVTecDataset(Dataset):
+    def __init__(self, root_path='../data', class_name='bottle', is_train=True,
+                 resize=256, cropsize=224):
+        assert class_name in CLASS_NAMES, 'class_name: {}, should be in {}'.format(class_name, CLASS_NAMES)
+        self.root_path = root_path
+        self.class_name = class_name
+        self.is_train = is_train
+        self.resize = resize
+        self.cropsize = cropsize
+        self.mvtec_folder_path = os.path.join(root_path, 'mvtec_anomaly_detection')
+
+        # download dataset if not exist
+        self.download()
+
+        # load dataset
+        self.x, self.y, self.mask = self.load_dataset_folder()
+
+        # set transforms
+        self.transform_x = T.Compose([T.Resize(resize, Image.ANTIALIAS),
+                                      T.CenterCrop(cropsize),
+                                      T.ToTensor(),
+                                      T.Normalize(mean=[0.485, 0.456, 0.406],
+                                                  std=[0.229, 0.224, 0.225])])
+        self.transform_mask = T.Compose([T.Resize(resize, Image.NEAREST),
+                                         T.CenterCrop(cropsize),
+                                         T.ToTensor()])
+
+    def __getitem__(self, idx):
+        x, y, mask = self.x[idx], self.y[idx], self.mask[idx]
+
+        x = Image.open(x).convert('RGB')
+        x = self.transform_x(x)
+
+        if y == 0:
+            mask = torch.zeros([1, self.cropsize, self.cropsize])
+        else:
+            mask = Image.open(mask)
+            mask = self.transform_mask(mask)
+
+        return x, y, mask
+
+    def __len__(self):
+        return len(self.x)
+
+    def load_dataset_folder(self):
+        phase = 'train' if self.is_train else 'test'
+        x, y, mask = [], [], []
+
+        img_dir = os.path.join(self.mvtec_folder_path, self.class_name, phase)
+        gt_dir = os.path.join(self.mvtec_folder_path, self.class_name, 'ground_truth')
+
+        img_types = sorted(os.listdir(img_dir))
+        for img_type in img_types:
+
+            # load images
+            img_type_dir = os.path.join(img_dir, img_type)
+            if not os.path.isdir(img_type_dir):
+                continue
+            img_fpath_list = sorted([os.path.join(img_type_dir, f)
+                                     for f in os.listdir(img_type_dir)
+                                     if f.endswith('.png')])
+            x.extend(img_fpath_list)
+
+            # load gt labels
+            if img_type == 'good':
+                y.extend([0] * len(img_fpath_list))
+                mask.extend([None] * len(img_fpath_list))
+            else:
+                y.extend([1] * len(img_fpath_list))
+                gt_type_dir = os.path.join(gt_dir, img_type)
+                img_fname_list = [os.path.splitext(os.path.basename(f))[0] for f in img_fpath_list]
+                gt_fpath_list = [os.path.join(gt_type_dir, img_fname + '_mask.png')
+                                 for img_fname in img_fname_list]
+                mask.extend(gt_fpath_list)
+
+        assert len(x) == len(y), 'number of x and y should be same'
+
+        return list(x), list(y), list(mask)
+
+    def download(self):
+        """Download dataset if not exist"""
+
+        if not os.path.exists(self.mvtec_folder_path):
+            tar_file_path = self.mvtec_folder_path + '.tar.xz'
+            if not os.path.exists(tar_file_path):
+                print('Download MVTec AD dataset from MVTec ftp server')
+                wget.download(URL, tar_file_path)
+            print('unzip downloaded dataset: %s' % tar_file_path)
+            tar = tarfile.open(tar_file_path, 'r:xz')
+            tar.extractall(self.mvtec_folder_path)
+            tar.close()
+
+        return
---
+++ src/main.py
@@ -0,0 +1,184 @@
+import argparse
+import numpy as np
+import os
+import pickle
+from tqdm import tqdm
+from collections import OrderedDict
+from sklearn.metrics import roc_auc_score, roc_curve
+from scipy.ndimage import gaussian_filter
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision.models import wide_resnet50_2
+
+import datasets.mvtec as mvtec
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('SPADE')
+    parser.add_argument("--top_k", type=int, default=5)
+    parser.add_argument("--save_path", type=str, default="./result")
+    return parser.parse_args()
+
+
+def main():
+
+    args = parse_args()
+
+    # device setup
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # load model
+    model = wide_resnet50_2(pretrained=True, progress=True)
+    model.to(device)
+    model.eval()
+
+    # set model's intermediate outputs
+    outputs = []
+    def hook(module, input, output):
+        outputs.append(output)
+    model.layer1[-1].register_forward_hook(hook)
+    model.layer2[-1].register_forward_hook(hook)
+    model.layer3[-1].register_forward_hook(hook)
+    model.avgpool.register_forward_hook(hook)
+
+    os.makedirs(os.path.join(args.save_path, 'temp'), exist_ok=True)
+
+    fig, ax = plt.subplots(1, 2, figsize=(20, 10))
+    fig_img_rocauc = ax[0]
+    fig_pixel_rocauc = ax[1]
+
+    total_roc_auc = []
+    total_pixel_roc_auc = []
+
+    for class_name in mvtec.CLASS_NAMES:
+
+        train_dataset = mvtec.MVTecDataset(class_name=class_name, is_train=True)
+        train_dataloader = DataLoader(train_dataset, batch_size=32, pin_memory=True)
+        test_dataset = mvtec.MVTecDataset(class_name=class_name, is_train=False)
+        test_dataloader = DataLoader(test_dataset, batch_size=32, pin_memory=True)
+
+        train_outputs = OrderedDict([('layer1', []), ('layer2', []), ('layer3', []), ('avgpool', [])])
+        test_outputs = OrderedDict([('layer1', []), ('layer2', []), ('layer3', []), ('avgpool', [])])
+        gt_list = []
+        gt_mask_list = []
+
+        # extract train set features
+        train_feature_filepath = os.path.join(args.save_path, 'temp', 'train_%s.pkl' % class_name)
+        if not os.path.exists(train_feature_filepath):
+            for (x, y, mask) in tqdm(train_dataloader, '| feature extraction | train | %s |' % class_name):
+                # model prediction
+                with torch.no_grad():
+                    pred = model(x.to(device))
+                # get intermediate layer outputs
+                for k, v in zip(train_outputs.keys(), outputs):
+                    train_outputs[k].append(v)
+                # initialize hook outputs
+                outputs = []
+            for k, v in train_outputs.items():
+                train_outputs[k] = torch.cat(v, 0)
+            # save extracted feature
+            with open(train_feature_filepath, 'wb') as f:
+                pickle.dump(train_outputs, f)
+        else:
+            print('load train set feature from: %s' % train_feature_filepath)
+            with open(train_feature_filepath, 'rb') as f:
+                train_outputs = pickle.load(f)
+
+        # extract test set features
+        for (x, y, mask) in tqdm(test_dataloader, '| feature extraction | test | %s |' % class_name):
+            gt_list.extend(y.cpu().detach().numpy())
+            gt_mask_list.extend(mask.cpu().detach().numpy())
+            # model prediction
+            with torch.no_grad():
+                pred = model(x.to(device))
+            # get intermediate layer outputs
+            for k, v in zip(test_outputs.keys(), outputs):
+                test_outputs[k].append(v)
+            # initialize hook outputs
+            outputs = []
+        for k, v in test_outputs.items():
+            test_outputs[k] = torch.cat(v, 0)
+
+        # calculate distance matrix
+        dist_matrix = calc_dist_matrix(torch.flatten(test_outputs['avgpool'], 1),
+                                       torch.flatten(train_outputs['avgpool'], 1))
+
+        # select K nearest neighbor and take average
+        topk_values, topk_indexes = torch.topk(dist_matrix, k=args.top_k, dim=1, largest=False)
+        scores = torch.mean(topk_values, 1).cpu().detach().numpy()
+
+        # calculate image-level ROC AUC score
+        fpr, tpr, _ = roc_curve(gt_list, scores)
+        roc_auc = roc_auc_score(gt_list, scores)
+        total_roc_auc.append(roc_auc)
+        print('%s ROCAUC: %.3f' % (class_name, roc_auc))
+        fig_img_rocauc.plot(fpr, tpr, label='%s ROCAUC: %.3f' % (class_name, roc_auc))
+
+        score_map_list = []
+        for t_idx in tqdm(range(test_outputs['avgpool'].shape[0]), '| localization | test | %s |' % class_name):
+            score_maps = []
+            for layer_name in ['layer1', 'layer2', 'layer3']:  # for each layer
+
+                # construct a gallery of features at all pixel locations of the K nearest neighbors
+                topk_feat_map = train_outputs[layer_name][topk_indexes[t_idx]]
+                test_feat_map = test_outputs[layer_name][t_idx:t_idx + 1]
+                feat_gallery = topk_feat_map.transpose(3, 1).flatten(0, 2).unsqueeze(-1).unsqueeze(-1)
+
+                # calculate distance matrix
+                dist_matrix_list = []
+                for d_idx in range(feat_gallery.shape[0] // 100):
+                    dist_matrix = torch.pairwise_distance(feat_gallery[d_idx * 100:d_idx * 100 + 100], test_feat_map)
+                    dist_matrix_list.append(dist_matrix)
+                dist_matrix = torch.cat(dist_matrix_list, 0)
+
+                # k nearest features from the gallery (k=1)
+                score_map = torch.min(dist_matrix, dim=0)[0]
+                score_map = F.interpolate(score_map.unsqueeze(0).unsqueeze(0), size=224,
+                                          mode='bilinear', align_corners=False)
+                score_maps.append(score_map)
+
+            # average distance between the features
+            score_map = torch.mean(torch.cat(score_maps, 0), dim=0)
+
+            # apply gaussian smoothing on the score map
+            score_map = gaussian_filter(score_map.squeeze().cpu().detach().numpy(), sigma=4)
+            score_map_list.append(score_map)
+
+        flatten_gt_mask_list = np.concatenate(gt_mask_list).ravel()
+        flatten_score_map_list = np.concatenate(score_map_list).ravel()
+
+        # calculate per-pixel level ROCAUC
+        fpr, tpr, _ = roc_curve(flatten_gt_mask_list, flatten_score_map_list)
+        per_pixel_rocauc = roc_auc_score(flatten_gt_mask_list, flatten_score_map_list)
+        total_pixel_roc_auc.append(per_pixel_rocauc)
+        print('%s pixel ROCAUC: %.3f' % (class_name, per_pixel_rocauc))
+        fig_pixel_rocauc.plot(fpr, tpr, label='%s ROCAUC: %.3f' % (class_name, per_pixel_rocauc))
+
+    print('Average ROCAUC: %.3f' % np.mean(total_roc_auc))
+    fig_img_rocauc.title.set_text('Average image ROCAUC: %.3f' % np.mean(total_roc_auc))
+    fig_img_rocauc.legend(loc="lower right")
+
+    print('Average pixel ROCUAC: %.3f' % np.mean(total_pixel_roc_auc))
+    fig_pixel_rocauc.title.set_text('Average pixel ROCAUC: %.3f' % np.mean(total_pixel_roc_auc))
+    fig_pixel_rocauc.legend(loc="lower right")
+
+    fig.tight_layout()
+    fig.savefig(os.path.join(args.save_path, 'roc_curve.png'), dpi=100)
+
+
+def calc_dist_matrix(x, y):
+    """Calculate Euclidean distance matrix with torch.tensor"""
+    n = x.size(0)
+    m = y.size(0)
+    d = x.size(1)
+    x = x.unsqueeze(1).expand(n, m, d)
+    y = y.unsqueeze(0).expand(n, m, d)
+    dist_matrix = torch.sqrt(torch.pow(x - y, 2).sum(2))
+    return dist_matrix
+
+
+if __name__ == '__main__':
+    main()
